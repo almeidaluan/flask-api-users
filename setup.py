89ea71846bd50a011bfe46248edfe6e929030f00
luan <almeida.luan099@gmail.com>
2019-04-27 17:31:21 -0300

    removendo url git setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     license='MIT',
     description=__description__,
     long_description=__long_description__,
-    url='https://github.com/lucassimon/flask-api-users.git',
+    url='',
     keywords='API, MongoDB',
     include_package_data=True,
     zip_safe=False,
